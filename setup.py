8a067cf6e2e7ea09fea419f689d893ec7dce4aa4
Keoni Gandall <koeng101@gmail.com>
2021-04-05 13:54:10 -0700

    Added Tim as author in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     name='opentronsfastapi',
     version='0.0.1',
     url='https://github.com/Koeng101/opentronsfastapi',
-    author='Keoni Gandall',
-    author_email='keoni@sporenetlabs.com',
+    author='Keoni Gandall, Tim Dobbs',
+    author_email='keoni@sporenetlabs.com, timdobbs@gmail.com',
     description='opentronsfastapi is package for building FastAPIs that run protocols on an Opentrons',
     long_description=long_description,
     packages=find_packages()    
